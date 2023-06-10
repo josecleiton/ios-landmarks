c8f482695f3bd0dbf9ff327ea6cc33c9e5594088
José Cleiton Correia <jose.correia@cubos.io>
2023-06-09 22:13:12 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by José Cleiton Correia on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
